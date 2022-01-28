6c387630c1e52a94e94f8f6955eb5645ca9f8201
drkostas <georgiou.kostas94@gmail.com>
2022-01-28 09:57:02 -0500

    Initial commit

---
+++ cloud_filemanager/__init__.py
@@ -0,0 +1,9 @@
+"""cloud-filemanager package"""
+
+from .dropbox_cloudmanager import DropboxCloudManager
+from .cloud_config import CloudConfig
+from termcolor_logger import ColorLogger
+
+__author__ = "drkostas"
+__email__ = "georgiou.kostas94@gmail.com"
+__version__ = "0.1.0"
